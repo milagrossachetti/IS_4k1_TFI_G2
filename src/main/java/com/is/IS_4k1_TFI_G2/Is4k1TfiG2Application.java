d310be431013cb16bb6bb9a6d676911d47c136fd
Milagros Sachetti <milagrossachetti@gmail.com>
2024-09-08 17:07:07 -0300

    Agrego las dependencias y la configuracion para conectarse a la base de datos local

---
+++ src/main/java/com/is/IS_4k1_TFI_G2/Is4k1TfiG2Application.java
@@ -0,0 +1,13 @@
+package com.is.IS_4k1_TFI_G2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is4k1TfiG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Is4k1TfiG2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/is/IS_4k1_TFI_G2/Is4k1TfiG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is.IS_4k1_TFI_G2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Is4k1TfiG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
