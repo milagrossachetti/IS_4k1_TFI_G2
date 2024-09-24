6dfc2a937e23632b03580b858a2ac6a43699ed8a
BeLuciaN-hub <marinellibrendaluciana@gmail.com>
2024-09-24 14:09:20 -0300

    Evolucion-GestionarPaciente1.1

--- src/main/java/com/is/IS_4k1_TFI_G2/controlador/ControladorPaciente.java
+++ src/main/java/com/is/IS_4k1_TFI_G2/controlador/ControladorPaciente.java
@@ -1,5 +1,6 @@
 package com.is.IS_4k1_TFI_G2.controlador;
 
+import com.is.IS_4k1_TFI_G2.modelos.Paciente;
 import com.is.IS_4k1_TFI_G2.servicios.ServicioPaciente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
